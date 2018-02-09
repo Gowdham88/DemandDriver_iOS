d9717cdf2f59bdb10e6e0ed4fac8524607875dd9
Suraj B <suraj@czsm.co.in>
2018-02-09 17:01:22 +0530

    image attach

--- DemandDriver/DemandDriver/Controller/otp.swift
+++ DemandDriver/DemandDriver/Controller/otp.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class otp: UIViewController {
 
+    @IBOutlet weak var viewTop: UIView!
+    @IBOutlet weak var imgTop: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var otpView: UIView!
+    @IBOutlet weak var otpText: UITextField!
+    @IBOutlet weak var resetView: UIView!
+    @IBOutlet weak var resetlbl: UILabel!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var loginLbl: UILabel!
+    @IBOutlet weak var btmView: UIView!
+    @IBOutlet weak var btmImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
