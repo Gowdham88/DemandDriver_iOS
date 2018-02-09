d19b84e355e190577e0adbb4f8667c563b4af58a
Suraj B <suraj@czsm.co.in>
2018-02-09 16:18:05 +0530

    designed OTP screen for demand driver

---
+++ DemandDriver/DemandDriver/Controller/otp.swift
@@ -0,0 +1,26 @@
+//
+//  otp.swift
+//  DemandDriver
+//
+//  Created by Suraj B on 09/02/2018.
+//  Copyright © 2018 Siva. All rights reserved.
+//
+
+import UIKit
+
+class otp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
