32a1b0866ca4e2949d514c490c420cb39034380a
Srinivasan <srinivasancse1994@gmail.com>
2018-03-21 11:43:16 +0530

    Keyboard avoid scrolling

--- DemandDriver/DemandDriver/AppDelegate.swift
+++ DemandDriver/DemandDriver/AppDelegate.swift
@@ -27,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        IQKeyboardManager.sharedManager().enable = true
         
 
        // GMSServices.provideAPIKey("AIzaSyB-SuyHSWWYfiRYJ49ph9ns8GGSSu9IQpQ")
